a38223658b755257778f0a79e5502a8425a4e73e
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-09-19 02:05:30 +0500

    Update Bangladesh_Flag.jsx

--- flags_project/src/components/Bangladesh_Flag.jsx
+++ flags_project/src/components/Bangladesh_Flag.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Bangladesh_Flag() {
   return (
-    <div className='bg-green-800 h-70 w-120 flex justify-center items-center'>
+    <div className='bg-green-700 h-70 w-120 flex justify-center items-center'>
         <div className='bg-red-600 h-35 w-35 rounded-full -ml-30'></div>
     </div>
   )
